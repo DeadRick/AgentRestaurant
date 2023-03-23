1319a02e1bbf3cf23503b85a8e58b96a0a4845a9
Shamil <dnumkfde2203@mail.ru>
2023-03-23 19:45:03 +0500

    add function sum_numbers

--- org/example/Main.java
+++ org/example/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Victor from my Branch!");
     }
+
+    int sum_numbers(int one, int second) {
+        return one + second;
+    }
 }
