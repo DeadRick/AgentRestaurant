2f719ef91be8b5dbf6a2f1ee36a9cf26ac8b4c86
ksofa <39325834+DeadRick@users.noreply.github.com>
2023-03-23 17:45:14 +0300

    added new function print_22()

--- org/example/Main.java
+++ org/example/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Victor from my Branch!");
     }
+
+    void print_22() {
+        System.out.println(22);
+    }
 }
